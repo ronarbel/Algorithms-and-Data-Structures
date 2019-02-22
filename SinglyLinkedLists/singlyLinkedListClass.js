fb947a590525d8d9d5a10e606a24f271045c78db
Ron Arbel <ronarbel1@gmail.com>
2019-02-21 23:54:50 -0800

    Complete singly linked lists

--- SinglyLinkedLists/singlyLinkedListClass.js
+++ SinglyLinkedLists/singlyLinkedListClass.js
@@ -67,14 +67,74 @@ class SinglyLinkedList {
     }
     this.length += 1;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter += 1;
+    }
+    return current;
+  }
+
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.value = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index >= this.length) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length - 1) return !!this.push(val);
+
+    const newNode = new Node(val);
+    const prevNode = this.get(index - 1);
+    newNode.next = prevNode.next;
+    prevNode.next = newNode;
+    this.length += 1;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    const previousNode = this.get(index - 1);
+    const removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length -= 1;
+    return removed;
+  }
+
+  reverse() {
+    if (this.length < 2) return this;
+    this.tail = this.head;
+    let newNext = this.head;
+    let current = this.head.next;
+    let oldNext = current.next;
+    while (oldNext) {
+      current.next = newNext;
+      newNext = current;
+      current = oldNext;
+      oldNext = oldNext.next;
+    }
+    current.next = newNext;
+    this.head = current;
+    this.tail.next = null;
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
-list.push(1);
-list.push(2);
-list.push(3);
-let three = list.pop();
-
+// list.push(1);
+// list.push(2);
+// list.push(3);
 
 console.log(list);
-console.log(three);
+console.log(list.reverse());
