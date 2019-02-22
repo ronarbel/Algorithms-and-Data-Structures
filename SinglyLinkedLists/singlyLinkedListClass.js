3777a715e3c5edee0af136c3b4eec61cc17553ea
Ron Arbel <ronarbel1@gmail.com>
2019-02-21 22:24:55 -0800

    Add pop method

--- SinglyLinkedLists/singlyLinkedListClass.js
+++ SinglyLinkedLists/singlyLinkedListClass.js
@@ -24,12 +24,32 @@ class SinglyLinkedList {
     this.length += 1;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    while (current.next.next) {
+      current = current.next;
+    }
+    const popped = this.tail;
+    this.tail = current;
+    this.tail.next = null;
+    this.length -= 1;
+    if (!this.length) {
+      this.head = null;
+      this.tail = null;
+    }
+    return popped;
+  }
 }
 
 const list = new SinglyLinkedList();
 list.push(1);
 list.push(2);
 list.push(3);
+let three = list.pop();
 
 
 console.log(list);
+console.log(three);
