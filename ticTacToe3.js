0836624fdc12dc63a72180782a4da00391ce830e
Ron Arbel <ronarbel1@gmail.com>
2019-03-15 22:49:21 -0700

    Add printBoard

--- ticTacToe3.js
+++ ticTacToe3.js
@@ -13,9 +13,24 @@ let win = false;
 
 let playableMoves = 9;
 
+let board = [
+  ['1', '2', '3'],
+  ['4', '5', '6'],
+  ['7', '8', '9'],
+];
+
 // -------- utils -------- //
 
+const printBoard = () => {
+  console.log(board[0][0], '|', board[0][1], '|', board[0][2]);
+  console.log('---------');
+  console.log(board[1][0], '|', board[1][1], '|', board[1][2]);
+  console.log('---------');
+  console.log(board[2][0], '|', board[2][1], '|', board[2][2]);
+}
+
 const promptPlayer = () => {
+  printBoard();
   rl.question(`Question`, (answer) => {
     console.log(answer);
   
